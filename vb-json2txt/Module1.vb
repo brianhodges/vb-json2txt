517ce0686991aa679a2983a4ac1fda8e5e04611f
Brian Hodges <bhodges@PMIDSD.com>
2017-08-09 11:59:35 -0400

    initial code commit

---
+++ vb-json2txt/Module1.vb
@@ -0,0 +1,33 @@
+﻿Imports System
+Imports System.IO
+Imports System.Text
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Module Module1
+
+    Sub Main()
+        Dim json As String = File.ReadAllText("data.json")
+        Dim jsonResult = JsonConvert.DeserializeObject(json)
+        Dim fs As FileStream = File.Create("log.txt")
+
+        For Each obj As JObject In jsonResult
+            Dim p As New Person(obj.Item("id"), obj.Item("first_name"), obj.Item("last_name"),
+                                obj.Item("email"), obj.Item("btc_address"), obj.Item("ip_address"))
+
+            Dim id As Byte() = New UTF8Encoding(True).GetBytes("ID: " + p.ID + Environment.NewLine)
+            fs.Write(id, 0, id.Length)
+            Dim fn As Byte() = New UTF8Encoding(True).GetBytes("Full Name: " + p.FullName + Environment.NewLine)
+            fs.Write(fn, 0, fn.Length)
+            Dim em As Byte() = New UTF8Encoding(True).GetBytes("Email: " + p.Email + Environment.NewLine)
+            fs.Write(em, 0, em.Length)
+            Dim btc As Byte() = New UTF8Encoding(True).GetBytes("BTC Address: " + p.BTCAddress + Environment.NewLine)
+            fs.Write(btc, 0, btc.Length)
+            Dim ip As Byte() = New UTF8Encoding(True).GetBytes("IP Address: " + p.IPAddress + Environment.NewLine + Environment.NewLine)
+            fs.Write(ip, 0, ip.Length)
+        Next
+
+        fs.Close()
+    End Sub
+
+End Module
---
+++ vb-json2txt/Person.vb
@@ -0,0 +1,72 @@
+﻿Public Class Person
+    Private fname, lname, mail, btc, ip As String
+    Private guid As Integer
+
+    Public Sub New(ByVal ID As Integer, FirstName As String, LastName As String,
+                   Email As String, BTCAddress As String, IPAddress As String)
+        Me.ID = ID
+        Me.FirstName = FirstName
+        Me.LastName = LastName
+        Me.Email = Email
+        Me.BTCAddress = BTCAddress
+        Me.IPAddress = IPAddress
+    End Sub
+
+    Public Property ID() As String
+        Get
+            Return guid
+        End Get
+        Set(ByVal Value As String)
+            guid = Value
+        End Set
+    End Property
+
+    Public Property FirstName() As String
+        Get
+            Return fname
+        End Get
+        Set(ByVal Value As String)
+            fname = Value
+        End Set
+    End Property
+
+    Public Property LastName() As String
+        Get
+            Return lname
+        End Get
+        Set(ByVal Value As String)
+            lname = Value
+        End Set
+    End Property
+
+    Public Property Email() As String
+        Get
+            Return mail
+        End Get
+        Set(ByVal Value As String)
+            mail = Value
+        End Set
+    End Property
+
+    Public Property BTCAddress() As String
+        Get
+            Return btc
+        End Get
+        Set(ByVal Value As String)
+            btc = Value
+        End Set
+    End Property
+
+    Public Property IPAddress() As String
+        Get
+            Return ip
+        End Get
+        Set(ByVal Value As String)
+            ip = Value
+        End Set
+    End Property
+
+    Public Function FullName() As String
+        Return String.Format("{0} {1}", FirstName, LastName)
+    End Function
+End Class
